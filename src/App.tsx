37a497712c0b2c986e918855291b64019f5a9e5d
Brandon Millman <brandon.millman@gmail.com>
2020-05-31 19:11:05 -0700

    Remove the learn react link

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ function App() {
       <header className="App-header">
         <img src="carmine.png" className="App-logo" alt="logo" />
         <p>THIS IS DANIELLAS WEBSITE.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
