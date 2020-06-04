c2facd0e3c5d3c32ce19bbbdd14eb211fd683fef
Brandon Millman <brandon.millman@gmail.com>
2020-06-03 23:29:15 -0700

    Experiment with react-layered-image

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import LayeredImage from "react-layered-image";
 import React from "react";
+
 import "./App.css";
+
+const layers = [
+  "carmine_background.png",
+  "carmine_smoke.png",
+  "carmine_action.png",
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="carmine.png" className="App-logo" alt="logo" />
-        <p>THIS IS DANIELLAS WEBSITE.</p>
-      </header>
+      <LayeredImage
+        layers={layers}
+        aspectRatio={1}
+        borderRadius={0}
+        lightOpacity={0}
+        style={{ width: "80%" }}
+      />
     </div>
   );
 }
